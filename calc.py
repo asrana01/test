0ad52f9114fca6aa3e402fa165a7596a425a7b0b
asrana <58824315+asrana01@users.noreply.github.com>
2025-07-23 15:55:01 -0700

    Add simple calculator and unit test

---
+++ calc.py
@@ -0,0 +1,9 @@
+"""A small calculator module."""
+
+def add(a, b):
+    """Return the sum of a and b."""
+    return a + b
+
+if __name__ == "__main__":
+    # Example usage
+    print("2 + 2 =", add(2, 2))
